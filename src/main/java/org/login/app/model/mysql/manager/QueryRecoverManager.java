37779aaf888bed6afb7e8a717e5ebca79480def7
DelRioV <pablosalvadordelriovergara.sanjose@alumnado.fundacionloyola.net>
2022-05-18 21:33:53 +0200

    Modifying QueryRecoverManager

--- src/main/java/org/login/app/model/mysql/manager/QueryRecoverManager.java
+++ src/main/java/org/login/app/model/mysql/manager/QueryRecoverManager.java
@@ -1,4 +1,10 @@
 package org.login.app.model.mysql.manager;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public interface QueryRecoverManager {
-}
+
+    public  ArrayList<ArrayList<String>> executeQuery(Connection connection, String query) ;
+
+    }
--- src/main/java/org/login/app/model/mysql/manager/imp/QueryRecoverManagerImp.java
+++ src/main/java/org/login/app/model/mysql/manager/imp/QueryRecoverManagerImp.java
@@ -1,4 +1,39 @@
 package org.login.app.model.mysql.manager.imp;
 
-public class QueryRecoverManagerImp {
+import org.login.app.model.mysql.manager.QueryRecoverManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class QueryRecoverManagerImp implements QueryRecoverManager {
+
+    @Override
+    public  ArrayList<ArrayList<String>> executeQuery(Connection connection, String query) {
+        ArrayList<ArrayList<String>> recoverQuery = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query+";");
+            while (rs.next()){
+                int index = 1;
+                ArrayList<String> addInfo = new ArrayList<>();
+                try{
+                    while(true) {
+                        addInfo.add(rs.getString(index));
+                        index++;
+                    }
+                } catch (Exception e){
+                    recoverQuery.add(addInfo);
+                }
+            }
+            return recoverQuery;
+        } catch (SQLException e) {
+            System.out.println("Something introduced isn't correct");
+            return null;
+        }
+    }
+
+    private void countIndex(ResultSet rs, int index) {
+
+    }
 }
